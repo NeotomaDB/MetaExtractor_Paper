a62288b610ac5ea272e526a3969c78d365213630
SimonGoring <goring@wisc.edu>
2023-08-31 15:33:44 -0700

    Modified loaddata to use the s3 bucket without exposing secrets.

--- src/loaddata.R
+++ src/loaddata.R
@@ -1,8 +1,8 @@
 get_datasetfile <- function() {
     key <- Sys.getenv("AWS_ACCESS_KEY_ID")
     secret <- Sys.getenv("AWS_SECRET_ACCESS_KEY")
-    path <- paste0("s3://", key, ":", secret, "@metareview/neotomaPredictedDF.parquet")
+    path <- paste0("s3://", key, ":", secret, "@metareview/neotomaMetadata.parquet")
 
-    paper_dataset <- read_parquet(path)
+    paper_dataset <- read_parquet(path) %>% as.data.frame()
     return(paper_dataset)
 }
